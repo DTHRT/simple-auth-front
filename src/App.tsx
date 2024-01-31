5b2a5d7192519c02e3fb5246863cb71ac7f9b077
Dmitriy Yoo <dmitriy2002yoo@gmail.com>
2024-01-31 22:25:15 +0900

    sex

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-    const submitHandler = async () => {
-        const response = await fetch('/auth/google', { method: 'POST' })
-        if (!response.ok) return
-        console.log(response.json())
-    }
+    useEffect(() => {
+        const queryString = window.location.search
+        const urlParams = new URLSearchParams(queryString)
+        const code = urlParams.get('code')
+
+        if (!code) return
+
+        fetch(`url`, { method: 'POST' })
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+    }, [])
 
     return (
         <form>
             <h2>Auth form</h2>
-            <button onClick={submitHandler}>Sign with Google</button>
+            <a href="google.com">Sign with Google</a>
         </form>
     )
 }
